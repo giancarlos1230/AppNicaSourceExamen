23dd545ddbd3b6b69f6a5ed30f72112a00be246d
giancarlos1230 <gianjiron98@gmail.com>
2021-11-29 22:42:58 -0600

    nuevas funcionalidades dentro del archivo Calificaciones

--- src/screen/Calificaciones.tsx
+++ src/screen/Calificaciones.tsx
@@ -1,16 +1,40 @@
-import React, { useState } from 'react'
-import { ScrollView, StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState, useEffect } from 'react'
+import { ScrollView, StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import FormInput from '../components/FormInput';
 
 const Calificacion = ({ navigation }) => {
 
-    const [nota, setNota] = useState<string>('')
+    const [dato, setDato] = useState<string>('')
     const [calis, setCalis] = useState<string[]>([])
+    const [pripar, setPripar] = useState('')
+    const [segpar, setSegpar] = useState('')
+    const [nfinal, setNfinal] = useState<string[]>([])
+    const [nfinales, setNfinales] = useState<string[]>([])
+
+    let pass: number = 0
+    let Validado = ''
+    let reprobo = 'SD'
 
     const crear = () => {
-        setCalis([...calis, nota])
+        setCalis([...calis, dato])
 
+        let notaFinal  = parseFloat(pripar) + parseFloat(segpar)
+        pass = notaFinal  / 2
+        validar()
     }
 
+    const validar = () => {
+        if (pripar === 'SD' || segpar === 'SD') {
+            Validado = reprobo
+            setNfinal([...nfinal, Validado])
+        }
+        if (pass >= 60) {
+            setNfinal([...nfinal, pass.toString()])
+        
+        }
+    }
+
+   //useEffect(crear, [pripar, segpar])
     return (
         <ScrollView>
             <View>
@@ -19,53 +43,40 @@ const Calificacion = ({ navigation }) => {
                     <TextInput
                         style={styles.inputs}
                         placeholder={'Nombre del estudiante'}
-                        onChangeText={setNota}
-                    />
-                </View>
-                <View style={styles.boton}
->
-
-                    <Button
-                    
-                        color="#05786A"
-                        title="Agregar"
-                        onPress={crear} />
-
-                </View>
-
-
-                <Text style={styles.text}>1P</Text>
-            <View style={styles.IP}>
-                <TextInput
-                    style={styles.inputs}
-                    placeholder={'1P'}
-                // onChangeText={setCalis}
-                />
-</View>
-
-
-                {/* <Text style={styles.text}>IIP</Text>
-                <View style={styles.IIP}>
-                    <TextInput
-                        style={styles.inputs}
-                        placeholder={'IIP'}
-                    // onChangeText={setCalis}
+                        onChangeText={setDato}
                     />
                 </View>
 
-                <Text style={styles.text}>IIIP</Text>
-                <View style={styles.IIIP}>
-                    <TextInput
-                        style={styles.inputs}
-                        placeholder={'IIP'}
-                    // onChangeText={setCalis}
-                    />
-                </View> */}
+
+                <View style={styles.Parciales}>
+                    <FormInput
+                        title={"IP"}
+                        placeholder={"IP"}
+                        onChangeText={setPripar} />
+
+                    <FormInput
+                        title={"IP"}
+                        placeholder={"IP"}
+                        onChangeText={setSegpar} />
+                </View>
+
+                <Text style={styles.text}> NF</Text>
+                    <Text style={styles.NF}>{nfinal}</Text>
+                   
+
+                <TouchableOpacity
+                    onPress={crear}
+                    style={styles.botonReset}
+                >
+                    <Text style={styles.textbotonReset}>Agregar</Text>
+                </TouchableOpacity>
 
                 {
-                    calis.map((lista, index) => (
+                    calis.map((calis, index) => (
                         <View style={styles.container} key={index} >
-                            <Text style={styles.text}>{lista}</Text>
+                            <Text style={styles.text}>{calis}</Text>
+                            <Text style={[nfinales[index] < '60' ? styles.textReprobo : styles.textAprobar]}>Nota Final :{notaFinales[index]}</Text>
+
                         </View>
                     ))
                 }
@@ -80,9 +91,38 @@ export default Calificacion
 
 const styles = StyleSheet.create({
 
-    
+    botonReset: {
+        backgroundColor: '#8C8A8A',
+        borderRadius: 8,
+        width: '30%',
+        paddingVertical: 7,
+        marginTop: 5
+    },
 
-    IP: {
+    textbotonReset: {
+        color: '#05786A',
+        fontWeight: 'bold',
+        fontSize: 20,
+        textAlign: 'center',
+        marginTop: 10
+    },
+
+    Parciales: {
+
+        width: '95%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 8,
+        borderRadius: 10,
+        marginLeft: 10,
+        borderWidth: 3,
+        borderColor: '#8C8A8A',
+        marginTop: 10,
+    },
+
+    NF: {
 
         width: '15%',
         flexDirection: 'row',
@@ -93,42 +133,6 @@ const styles = StyleSheet.create({
         marginLeft: 10,
         borderWidth: 2,
         borderColor: '#8C8A8A',
-    },
-
-    // IIP: {
-
-    //     width: '30%',
-    //     flexDirection: 'row',
-    //     justifyContent: 'space-between',
-    //     marginBottom: 10,
-    //     paddingVertical: 8,
-    //     paddingHorizontal: 8,
-    //     marginLeft: 10,
-    //     borderWidth: 2,
-    //     borderColor: '#8C8A8A',
-    // },
-
-    // NF: {
-
-    //     width: '15%',
-    //     flexDirection: 'row',
-    //     justifyContent: 'space-between',
-    //     marginBottom: 10,
-    //     paddingVertical: 10,
-    //     paddingHorizontal: 8,
-    //     marginLeft: 10,
-    //     borderWidth: 2,
-    //     borderColor: '#8C8A8A',
-    // },
-
-    separator: {
-        marginVertical: 8,
-        borderBottomColor: "#737373",
-        borderBottomWidth: StyleSheet.hairlineWidth,
-      },
-
-    boton: {
-        backgroundColor: '#8C8A8A'
     },
 
     container: {
@@ -145,10 +149,18 @@ const styles = StyleSheet.create({
 
     },
     text: {
-        fontSize: 20,
+        fontSize: 24,
         color: '#05786A',
-        paddingVertical: 10,
-        paddingHorizontal: 10,
+    },
+
+    textReprobo: {
+        fontSize: 24,
+        color: '#B52222',
+    },
+
+    textAprobar: {
+        fontSize: 24,
+        color: '#05786A',
     },
 
     inputs: {
